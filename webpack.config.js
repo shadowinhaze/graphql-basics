fd90639e41d1bcf974b3df64152d99549229ff55
@shadowinhaze <shadowinhaze@gmail.com>
2022-06-29 22:33:17 +0200

    init: start graphql project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'node',
+  externals: [
+    {
+      robotjs: 'commonjs robotjs',
+    },
+    'bufferutil',
+    'utf-8-validate',
+  ],
+};
